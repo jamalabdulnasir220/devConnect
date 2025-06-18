c65224f56687ebaed475389fc4b408d55cdf53cb
unknown <jamalnasirone52@gmail.com>
2025-06-18 12:29:35 +0000

    geeksforgeeks middleware example

--- src/app.js
+++ src/app.js
@@ -2,18 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/some",
-  (req, res, next) => {
-    console.log("This is the first route handler");
-    // res.send("First route handler");
-     next();
-  },
-  (req, res) => {
-    console.log("This is the second route handler");
-    res.send("Second route handler");
+const authenticateMiddleware = (req, res, next) => {
+  if (req.headers.authorization === "Bearer token") {
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
   }
-);
+};
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+};
+
+app.get("/protected", authenticateMiddleware, loggerMiddleware, (req, res) => {
+  res.send("Protected Route");
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
