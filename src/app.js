5775b7a71f955e528ad0a7b60340308a51098a33
unknown <jamalnasirone52@gmail.com>
2025-06-18 05:30:44 +0000

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,34 +2,18 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/hello", (req, res) => {
-//   res.send("This is the hello hello hello");
-// });
-// app.use("/hello/23", (req, res) => {
-//   res.send("This is difference");
-// });
-app.get("/user/:userId", (req, res) => {
- console.log(req.params)
-//  console.log(req.query)
-  res.send({ firstName: "Jamal", lastName: "Nasir" });
-});
-
-app.post("/user", (req, res) => {
-  // Save data to the database
-  res.send("Data saved to the database successfully");
-});
-app.delete("/user", (req, res) => {
-  // delete data to the database
-  res.send("Data deleted from the database successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("This the test server.");
-});
-
-// app.use("/", (req, res) => {
-//   res.send("This is the dashboard");
-// });
+app.use(
+  "/some",
+  (req, res, next) => {
+    console.log("This is the first route handler");
+    // res.send("First route handler");
+     next();
+  },
+  (req, res) => {
+    console.log("This is the second route handler");
+    res.send("Second route handler");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
