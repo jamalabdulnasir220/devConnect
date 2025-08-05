1cf14331408b4146410ae4bbba370c29ab4ca41f
unknown <jamalnasirone52@gmail.com>
2025-08-05 06:01:56 +0000

    added pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,8 @@ const User = require("../models/user");
 
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName photo age skills";
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -12,7 +14,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const receivedRequests = await ConnectionRequestModel.find({
       toUserId: loggedInUser._id,
       status: "Interested",
-    }).populate("fromUserId", ["firstName", "lastName", "photo", "age", "skills"]);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     if (receivedRequests.length === 0) {
       return res.status(404).send("No connection requests received");
@@ -41,6 +43,85 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(400).send(`ERROR: ${error.message}`);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connections = await ConnectionRequestModel.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "Accepted" },
+        { toUserId: loggedInUser._id, status: "Accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const connectionsWithDetails = connections.map((connection) => {
+      if (connection.fromUserId._id.equals(loggedInUser._id)) {
+        return connection.toUserId;
+      } else return connection.fromUserId;
+    });
+
+    if (connections.length === 0) {
+      return res.status(404).json({
+        message: "No connections found",
+      });
+    }
+    res.status(200).json({
+      message: "Connections retrieved successfully",
+      connections: connectionsWithDetails,
+    });
+  } catch (error) {
+    res.status(400).json({ message: `ERROR: ${error.message}` });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit; // Limit to a maximum of 50 results per page
+    const skip = (page - 1) * limit;
+
+    // For the feed,
+    // if we are logged in as user A,
+    // we shouldn't see the user A's profile,
+    // we should'nt see the connections the user A has interested, ignored, accepted, rejected
+
+    const connections = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+
+    connections.forEach((connection) => {
+      hideUsersFromFeed.add(connection.fromUserId.toString());
+      hideUsersFromFeed.add(connection.toUserId.toString());
+    });
+
+    console.log("Hide users from feed:", hideUsersFromFeed);
+
+    const feedUsers = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      message: "Feed retrieved successfully",
+      feedUsers,
+    });
+  } catch (error) {
+    res.status(400).json({ message: `ERROR: ${error.message}` });
   }
 });
 
