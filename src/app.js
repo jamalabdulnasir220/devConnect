dc111b136a8aa581b900aa2a490520fcb8968581
unknown <jamalnasirone52@gmail.com>
2025-08-28 15:20:20 +0000

    update

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ const app = express();
 // Load Swagger specification from YAML file
 // const swaggerSpec = YAML.load("./src/swagger.yaml");
 
+
 app.use(
   cors({
-    // origin: [
-    //   "https://gittogetherbuddies.onrender.com",
-    //   // "http://localhost:5173/",
-    // ],
+    origin: [
+      "https://gittogetherbuddies.onrender.com",
+      "http://localhost:5173",
+    ],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
