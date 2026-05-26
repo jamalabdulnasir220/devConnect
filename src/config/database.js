f9b4e3dd31a8df123056313319859cfd1277748a
unknown <jamalnasirone52@gmail.com>
2026-05-26 03:57:48 +0000

    update

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    process.env.DB_SECRET
+    process.env.DB_SECRET || "mongodb+srv://Jamal:Abdul8smwbb8@devconnect.x5j9kp0.mongodb.net/devConnect"
   );
 };
 
